cada54ace58512106ec43508d4f22a01ab8f08fc
Postrik <postrik@list.ru>
2022-10-10 11:46:32 +0500

    add Change Task_2 Home Work 5

--- HomeWork_5/Task_2.py
+++ HomeWork_5/Task_2.py
@@ -14,11 +14,20 @@ moves = []
 
 win = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
+
 def winCondition():
     for i in win:
-        if position[i[0]-1] == position[i[1]-1] == position[i[2]-1]:
-            print('Игра окончена')
+        if position[i[0]-1] == position[i[1]-1] == position[i[2]-1] == "O":
+            print('Игра окончена, победил игрок, ходящий "O"')
+            printField(position)
             exit()
+        elif position[i[0]-1] == position[i[1]-1] == position[i[2]-1] == "X":
+            print('Игра окончена, победил игрок, ходящий "X"')
+            printField(position)
+            exit()
+        
+        
+
 
 def printField(position):
     print(f'{position[0]:^5}|{position[1]:^5}|{position[2]:^5}')
@@ -41,5 +50,7 @@ while True:
                 sign = "O"
             moves.append(index)
             position[index-1] = sign
-            winCondition()
-            break
+            winCondition()            
+        if len(moves) == len(position):
+            print('Игра завершена, результат ничья')
+            exit()
